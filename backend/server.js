3437bb9bca2fdc6dea793cb705aaeab5468c29e1
abirc8010 <abirc8010@gmail.com>
2024-04-27 22:32:51 +0530

    Fixed errors

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,27 @@
 const app = require('express')();
-
 const server = require('http').createServer(app);
-
-
-const io = require('socket.io')(server,{
-    cors:{
-        origin:"*",
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
     }
 });
-    
+
 io.on("connection", (socket) => {
+    console.log("User connected");
+
     socket.on("chat", (payload) => {
         io.emit("chat", payload);
     });
 
-})
-server.listen(5000,()=>{
+    socket.on("typing", (data) => {
+        socket.broadcast.emit("notifyTyping", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
+
+server.listen(5000, () => {
     console.log("server is listening to port 5000 ...");
 });
- socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
-     socket.on("typing", data => {
-    socket.broadcast.emit("notifyTyping", { user: data.user, message: data.message });
-     }); 
-
-socket.on("stopTyping", () => { socket.broadcast.emit("notifyStopTyping"); });
