cd46a07267bbb766e117bf6c10cef6f65c65d513
abirc8010 <abirc8010@gmail.com>
2024-04-27 22:01:33 +0530

    Added a typing feature

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ const app = require('express')();
 
 const server = require('http').createServer(app);
 
+
 const io = require('socket.io')(server,{
     cors:{
         origin:"*",
     }
 });
-
+    
 io.on("connection", (socket) => {
     socket.on("chat", (payload) => {
         io.emit("chat", payload);
@@ -20,3 +21,8 @@ server.listen(5000,()=>{
  socket.on('disconnect', () => {
         console.log('A user disconnected');
     });
+     socket.on("typing", data => {
+    socket.broadcast.emit("notifyTyping", { user: data.user, message: data.message });
+     }); 
+
+socket.on("stopTyping", () => { socket.broadcast.emit("notifyStopTyping"); });
