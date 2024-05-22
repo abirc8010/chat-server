020ed75de5cd31f938872f43210f8650506edd7b
abirc8010 <abirc8010@gmail.com>
2024-05-22 22:41:52 +0530

    Changed url

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-mongoose.connect('mongodb://localhost/WebChat', {
+mongoose.connect(meta.env.SERVER_URL, {
 
 }).then(() => {
     console.log('Connected to MongoDB');
