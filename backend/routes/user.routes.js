b5f4549c46bc12771bf05c6bdf4aa897ba7a36f5
abirc8010 <abirc8010@gmail.com>
2024-10-11 10:11:44 +0530

    added add user api and handling media messages

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
-import { login,register,validateToken,getContacts,addContact,getMessagesBetweenUsers } from "../controllers/userControllers.js";
-const router= Router();
+import {
+  login,
+  register,
+  validateToken,
+  getContacts,
+  addContact,
+  getMessagesBetweenUsers,
+  getSearchResults,
+} from "../controllers/userControllers.js";
+const router = Router();
 router.route("/login").post(login);
 router.route("/register").post(register);
 router.route("/validate-token").post(validateToken);
 router.route("/contacts").get(getContacts);
 router.route("/add-contact").post(addContact);
 router.route("/messages").get(getMessagesBetweenUsers);
+router.route("/search").get(getSearchResults);
 export default router;
